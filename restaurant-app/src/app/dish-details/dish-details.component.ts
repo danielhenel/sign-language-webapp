aaca3d467d29461d4543368e9065d34b02617a38
YgLK <kuba21316@wp.pl>
2023-01-01 16:32:29 +0100

    Add image slider and review form to dish details view

--- restaurant-app/src/app/dish-details/dish-details.component.ts
+++ restaurant-app/src/app/dish-details/dish-details.component.ts
@@ -12,9 +12,11 @@ import {CartService} from "../shared/cart.service";
 export class DishDetailsComponent implements OnInit{
   dish: Dish;
   dishesCart: Map<Dish, number>;
+  // image for the slider
+  imageObject: Array<object> = new Array<object>();
 
   constructor(private dishService: DishService, private router: Router, private route: ActivatedRoute,
-              private cartService: CartService) {
+              public  cartService: CartService) {
     this.dishesCart = cartService.dishesCart;
     // force route reload whenever params change
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
@@ -25,6 +27,11 @@ export class DishDetailsComponent implements OnInit{
     this.dish = this.dishService.getDishByName(
       this.route.snapshot.params['id']
     )!;
+    this.dish.imageUrls.forEach(
+      (url: string) => {
+        this.imageObject.push({image: url, thumbImage: url});
+      }
+    );
   }
 
   addDishToCart(dish: Dish) {
