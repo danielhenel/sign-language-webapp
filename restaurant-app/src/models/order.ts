45d617c832087e5f4e32ea4f392da0bd4328a0c6
YgLK <kuba21316@wp.pl>
2023-01-16 05:54:07 +0100

    Use backend with mongodb for orders persistence

--- restaurant-app/src/models/order.ts
+++ restaurant-app/src/models/order.ts
@@ -6,4 +6,22 @@ export class Order {
     public dishes: Map<number, number>,
     public total: number,
   ){}
+
+  getOrderJson() {
+    class OrderEntry {
+      constructor(
+        public dishId: number,
+        public quantity: number
+      ){}
+    }
+    let entries = Array.from(this.dishes.entries()).map((entry) => new OrderEntry(entry[0], entry[1]));
+
+    return {
+      id: this.id,
+      userId: this.userId,
+      date: this.date,
+      dishes: entries,
+      total: this.total
+    };
+  }
 }
