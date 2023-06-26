793dcea8a2ef2b5dc5d32ad26cdb355caa57f1a3
YgLK <kuba21316@wp.pl>
2023-06-26 03:19:25 +0200

    Integrate detection service with frontend

--- sign-language/src/app/camera-capture/camera-capture.component.ts
+++ sign-language/src/app/camera-capture/camera-capture.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { WebcamImage, WebcamInitError, WebcamUtil } from 'ngx-webcam';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import cv from "@techstark/opencv-js";
+import {GameService} from "../shared/game.service";
 
 
 @Component({
@@ -12,42 +12,42 @@ import cv from "@techstark/opencv-js";
 })
 
 export class CameraCaptureComponent implements OnInit {
-  constructor(private http: HttpClient) {  }
   private trigger: Subject<any> = new Subject();
   public webcamImage!: WebcamImage;
   private nextWebcam: Subject<any> = new Subject();
   sysImage = '';
+
+  constructor(private http: HttpClient, private gameService: GameService) {}
+
   ngOnInit() {}
+
   public getSnapshot(): void {
     this.trigger.next(void 0);
-
   }
+
   public captureImg(webcamImage: WebcamImage): void {
     this.webcamImage = webcamImage;
     this.sysImage = webcamImage!.imageAsDataUrl;
+    // console.log(this.webcamImage.imageAsBase64);
 
-    // example
-    const gray = new cv.Mat();
-    var image = cv.imread(this.sysImage);
-    var gray_image = cv.cvtColor(image, gray,cv.COLOR_RGBA2GRAY,0);
-    console.info(gray_image);
-    //
-
-    console.info('got webcam image', this.sysImage);
-    
-
+    // get the prediction from the server
+    this.http.post(
+      '/api/classify',
+      {image: this.webcamImage.imageAsBase64},
+      {headers: new HttpHeaders( {'Content-Type': 'application/json'})}
+    ).subscribe((data: any) => {
+      console.log(data);
+      // check if letter in word and update game state
+      this.gameService.guessLetter(data.letter);
+      this.gameService.setLastGuess(data.letter, data.confidence);
+    });
   }
+
   public get invokeObservable(): Observable<any> {
     return this.trigger.asObservable();
   }
+
   public get nextWebcamObservable(): Observable<any> {
     return this.nextWebcam.asObservable();
-  }
-
-  public getDetection(){
-      this.http.get<any>('/api/detection')
-      .subscribe((data: any) => {
-          console.log(data);
-      });
   }
 }
--- sign-language/src/app/shared/game.service.ts
+++ sign-language/src/app/shared/game.service.ts
@@ -7,6 +7,7 @@ import {Alphabet} from "../../models/alphabet";
 export class GameService {
   alphabet: Alphabet;
   currentLetter: string = "";
+  currentLetterConfidence: number = 0;
   currentWord: string = "";
   currentImageUrl: string = "";
   lettersLeft: string[] = [];
@@ -48,6 +49,18 @@ export class GameService {
       this.isWordGuessed = true;
       this.isGameStarted = false;
     }
+  }
+
+  setLastGuess(letter, confidence){
+    this.currentLetter = letter;
+    this.currentLetterConfidence = confidence;
+  }
+
+  getLastGuess(){
+    // let approx_conf = (Math.round(this.currentLetterConfidence * 100)/100).toFixed(2); // round to 2 decimal places
+    let approx_conf = Math.round(this.currentLetterConfidence * 100);
+    console.log(approx_conf)
+    return this.currentLetter + " (" + approx_conf + "%)";
   }
 
   isLetterGuessed(letter: string): boolean {
